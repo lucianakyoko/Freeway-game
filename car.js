fe3feaa3fc3ccb8fcb2997f392b0fb9cb765a365
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-02-14 14:04:09 -0300

    Adding car loop

--- car.js
+++ car.js
@@ -25,3 +25,15 @@ function movimentaCarro() {
   xCarro2 -= velocidadeCarro2;
   xCarro3 -= velocidadeCarro3;
 }
+
+function voltaPosicaoInicialDoCarro() {
+  if(xCarro1 < -50) {
+    xCarro1 = 600;
+  }
+  if(xCarro2 < -50) {
+    xCarro2 = 600;
+  }
+  if(xCarro3 < -50) {
+    xCarro3 = 600;
+  }
+}
