337b4c52ed4d25f01ca67fe3ebf25a20ae5ab78e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-02-14 13:55:01 -0300

    Adding more cars

--- car.js
+++ car.js
@@ -1,11 +1,27 @@
-//Carro:
-let xCarro = 600;
-let yCarro = 40;
+//Carro 1:
+let xCarro1 = 600;
+let yCarro1 = 40;
+let velocidadeCarro1 = 2;
+
+//Carro 2:
+let xCarro2 = 600;
+let yCarro2 = 96;
+let velocidadeCarro2 = 3;
+
+//Carro 3:
+let xCarro3 = 600;
+let yCarro3 = 150;
+let velocidadeCarro3 = 2.5;
+
 
 function mostraCarro() {
-  image(imagemCarro, xCarro, yCarro, 60, 40);
+  image(imagemCarro1, xCarro1, yCarro1, 60, 40);
+  image(imagemCarro2, xCarro2, yCarro2, 60, 40);
+  image(imagemCarro3, xCarro3, yCarro3, 60, 40);
 }
 
 function movimentaCarro() {
-  xCarro -= 2;
+  xCarro1 -= velocidadeCarro1;
+  xCarro2 -= velocidadeCarro2;
+  xCarro3 -= velocidadeCarro3;
 }
--- images.js
+++ images.js
@@ -1,10 +1,14 @@
 // Carrega as imagens do jogo:
 let imagemDaEstrada;
 let imagemDoAtor;
-let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload() {
   imagemDaEstrada = loadImage('/images/estrada.png');
   imagemDoAtor = loadImage('/images/ator-1.png');
-  imagemCarro = loadImage('/images/carro-1.png');
+  imagemCarro1 = loadImage('/images/carro-1.png');
+  imagemCarro2 = loadImage('/images/carro-2.png');
+  imagemCarro3 = loadImage('/images/carro-3.png');
 }
